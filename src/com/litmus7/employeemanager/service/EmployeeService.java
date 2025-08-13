fc387e6563294edb5ac95f6ec14263abf718e430
habeeba <abhikm210@gmail.com>
2025-08-13 12:59:56 +0530

    update EmployeeService.java

--- src/com/litmus7/employeemanager/service/EmployeeService.java
+++ src/com/litmus7/employeemanager/service/EmployeeService.java
@@ -2,7 +2,7 @@ package com.litmus7.employeemanager.service;
 
 import com.litmus7.employeemanager.dao.employeeDAO;
 import com.litmus7.employeemanager.dto.Employee;
-import com.litmus7.employeemanager.constants.AppException;
+import com.litmus7.employeemanager.constants.exception;
 
 import java.util.List;
 
@@ -11,33 +11,34 @@ public class EmployeeService {
 
     private final employeeDAO dao = new employeeDAO();
     
-    public int createEmployee(Employee employee) throws AppException {
+    public int createEmployee(Employee employee) throws exception {
 		int rowInserted = dao.saveEmployee(employee);
-		if (rowInserted==0) throw new AppException("failed to insert employee");
+		if (rowInserted==0) throw new exception("failed to insert employee");
 		return rowInserted;
 	}
     
-    public List<Employee> getAllEmployees() throws AppException {
+    public List<Employee> getAllEmployees() throws exception {
 		List<Employee> employees = dao.selectAllEmployees();
-		if (employees.isEmpty()) throw new AppException("failed to retrieve employee list");
+		if (employees.isEmpty()) throw new exception("failed to retrieve employee list");
 		return employees;
 	}
 	
-    public Employee getEmployeeData(int id) throws AppException {
+    public Employee getEmployeeData(int id) throws exception {
 		Employee employee =dao.selectEmployeeById(id);
-		if (employee == null) throw new AppException("failed to retrieve employee details");
+		if (employee == null) throw new exception("failed to retrieve employee details");
 		return employee;
 	}
 
-	public int deleteEmployeeData(int id) throws AppException {
+	public int deleteEmployeeData(int id) throws exception {
 		int rowsDeleted = dao.deleteEmployeeById(id);
-		if (rowsDeleted==0) throw new AppException("failed to delete employee");
+		if (rowsDeleted==0) throw new exception("failed to delete employee");
 		return rowsDeleted;
 	}
 	
-	public int updateEmployee(Employee employee) throws AppException {
+	public int updateEmployee(Employee employee) throws exception {
 		int rowsUpdated = dao.updateEmployee(employee);
-		if (rowsUpdated==0) throw new AppException("failed to delete employee");
+		if (rowsUpdated==0) throw new exception("failed to delete employee");
 		return rowsUpdated;
 	}
+
 }
